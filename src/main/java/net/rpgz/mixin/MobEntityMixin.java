839960f5865285d2a3f2a15d7f0893f0e8273b68
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-07-23 03:02:22 +0100

    changed overwrite to redirect

--- src/main/java/net/rpgz/mixin/MobEntityMixin.java
+++ src/main/java/net/rpgz/mixin/MobEntityMixin.java
@@ -3,20 +3,19 @@ package net.rpgz.mixin;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.controller.BodyController;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import net.rpgz.access.IInventoryAccess;
 
@@ -34,23 +33,12 @@ public abstract class MobEntityMixin extends LivingEntity implements IInventoryA
   }
 
   // Stop turning after death
-  @Overwrite
-  public float updateDistance(float bodyRotation, float headRotation) {
+  @Inject(method = "updateDistance", at = @At("HEAD"), cancellable = true)
+  public void updateDistance(float bodyRotation, float headRotation, CallbackInfoReturnable<Float> info) {
     if (this.deathTime > 0) {
-      return 0.0F;
-    } else
-      this.bodyController.updateRenderAngles();
-    return headRotation;
+    	info.setReturnValue(0.0F);
+    }
   }
-
-  // @Inject(method = "dropSpecialItems", at = @At(value = "INVOKE", target =
-  // "Lnet/minecraft/entity/Entity;dropStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/ItemEntity;"))
-  // public void dropEquipmentMixin(ItemStack itemStack,DamageSource source, int
-  // lootingMultiplier, boolean allowDrops,CallbackInfo info) {
-  // this.addingInventoryItems(itemStack);
-  // info.cancel();
-  // }
-  // Lnet/minecraft/entity/mob/MobEntity;isInDaylight()Z
 
   @Inject(method = "Lnet/minecraft/entity/MobEntity;isInDaylight()Z", at = @At("HEAD"), cancellable = true)
   private void isInDaylightMixin(CallbackInfoReturnable<Boolean> info) {
@@ -59,28 +47,10 @@ public abstract class MobEntityMixin extends LivingEntity implements IInventoryA
     }
   }
 
-  @Overwrite
-  public void dropSpecialItems(DamageSource source, int lootingMultiplier, boolean allowDrops) {
-    super.dropSpecialItems(source, lootingMultiplier, allowDrops);
-    EquipmentSlotType[] var4 = EquipmentSlotType.values();
-    int var5 = var4.length;
-
-    for (int var6 = 0; var6 < var5; ++var6) {
-      EquipmentSlotType equipmentSlot = var4[var6];
-      ItemStack itemStack = this.getItemStackFromSlot(equipmentSlot);
-      float f = this.getDropChance(equipmentSlot);
-      boolean bl = f > 1.0F;
-      if (!itemStack.isEmpty() && !EnchantmentHelper.hasVanishingCurse(itemStack) && (allowDrops || bl)
-          && Math.max(this.rand.nextFloat() - (float) lootingMultiplier * 0.01F, 0.0F) < f) {
-        if (!bl && itemStack.isDamageable()) {
-          itemStack.setDamage(itemStack.getMaxDamage()
-              - this.rand.nextInt(1 + this.rand.nextInt(Math.max(itemStack.getMaxDamage() - 3, 1))));
-        }
-        this.addingInventoryItems(itemStack);
-        this.setItemStackToSlot(equipmentSlot, ItemStack.EMPTY);
-      }
-    }
-
+  @Redirect(method = "dropSpecialItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/MobEntity;entityDropItem(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/item/ItemEntity;"))
+  private ItemEntity dropSpecialItemsMixin(MobEntity mobEntity, ItemStack itemStack) {
+    this.addingInventoryItems(itemStack);
+    return null;
   }
 
   @Shadow
