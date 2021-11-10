cd17ca2fff2dd8709b9ba23e6d8009b450b262e5
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-11-10 23:11:12 +0100

    Missed a few changes

--- src/main/java/net/rpgz/mixin/misc/SquidEntityModelMixin.java
+++ src/main/java/net/rpgz/mixin/misc/SquidEntityModelMixin.java
@@ -6,28 +6,28 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.model.ListModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.SquidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.Entity;
 
 @Mixin(SquidModel.class)
-public abstract class SquidEntityModelMixin<T extends Entity> extends ListModel<T> {
-  @Shadow
-  private final ModelPart[] tentacles = new ModelPart[8];
-
-  //I guess uneccessary
-  @Inject(method = "setupAnim", at = @At(value = "RETURN"), cancellable = true)
-  public void setRotationAnglesMixin(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw,
-      float headPitch, CallbackInfo info) {
-    if (!entity.isAlive()) {
-      // ModelPart[] var7 = this.legs;
-      // int var8 = var7.length;
-      // for (int var9 = 0; var9 < var8; ++var9) {
-      // ModelPart modelPart = var7[var9];
-      // modelPart.pitch = 0.0F;
-      // }
-    }
-  }
+public abstract class SquidEntityModelMixin<T extends Entity> extends HierarchicalModel<T> {
+//  @Shadow
+//  private final ModelPart[] tentacles = new ModelPart[8];
+//
+//  //I guess uneccessary
+//  @Inject(method = "setupAnim", at = @At(value = "RETURN"), cancellable = true)
+//  public void setRotationAnglesMixin(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw,
+//      float headPitch, CallbackInfo info) {
+//    if (!entity.isAlive()) {
+//      // ModelPart[] var7 = this.legs;
+//      // int var8 = var7.length;
+//      // for (int var9 = 0; var9 < var8; ++var9) {
+//      // ModelPart modelPart = var7[var9];
+//      // modelPart.pitch = 0.0F;
+//      // }
+//    }
+//  }
 
 }
