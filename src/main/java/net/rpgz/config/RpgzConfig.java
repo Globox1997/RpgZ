8bf54c4ba5e76ae26f647965ce120d86618804d3
Globox1997 <mitschke11@web.de>
2022-01-18 11:12:33 +0100

    fixed tooltip

--- src/main/java/net/rpgz/config/RpgzConfig.java
+++ src/main/java/net/rpgz/config/RpgzConfig.java
@@ -20,7 +20,7 @@ public class RpgzConfig implements ConfigData {
     public int despawn_corps_after_ticks = 4800;
     public boolean despawn_immediately_when_empty = true;
     public boolean surfacing_in_water = true;
-    @Comment("Example: minecraft:zombie or adventurez.brown_fungus")
+    @Comment("Example: minecraft.zombie or adventurez.brown_fungus")
     public List<String> excluded_entities = new ArrayList<>();
 
 }
