b47338494118c5787e284aa673a74cb2742b8ff2
Stereowalker <stereowalker.mc@gmail.com>
2024-11-23 16:00:09 +0100

    Replaced resourcelocation initializer

--- src/main/java/net/rpgz/init/SoundInit.java
+++ src/main/java/net/rpgz/init/SoundInit.java
@@ -1,13 +1,15 @@
 package net.rpgz.init;
 
+import com.stereowalker.unionlib.util.VersionHelper;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.registries.RegisterEvent.RegisterHelper;
 
 public class SoundInit {
 
-  public static final ResourceLocation LOOT_SOUND = new ResourceLocation("rpgz:loot");
-  public static final ResourceLocation COIN_LOOT_SOUND = new ResourceLocation("rpgz:coin_loot");
+  public static final ResourceLocation LOOT_SOUND = VersionHelper.toLoc("rpgz:loot");
+  public static final ResourceLocation COIN_LOOT_SOUND = VersionHelper.toLoc("rpgz:coin_loot");
 
   public static SoundEvent LOOT_SOUND_EVENT = SoundEvent.createVariableRangeEvent(LOOT_SOUND);
   public static SoundEvent COIN_LOOT_SOUND_EVENT = SoundEvent.createVariableRangeEvent(COIN_LOOT_SOUND);
